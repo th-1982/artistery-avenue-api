c3bde087e611b4d839c3be6811c7096018b5f610
Theresa Wolff <theresawolff4@gmail.com>
2024-01-20 22:19:07 +0000

    Annotate Profile views' querysets

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -26,5 +26,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'following_id',
+            'content', 'image', 'is_owner', 'following_id', 'following_count',
         ]
