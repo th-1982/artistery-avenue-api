5045e0fd80931e60a362d8413fae82e10ad12dc3
Theresa Wolff <theresawolff4@gmail.com>
2024-01-21 02:09:42 +0000

    Add filter to commentList to filter by post id

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -13,6 +13,8 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
