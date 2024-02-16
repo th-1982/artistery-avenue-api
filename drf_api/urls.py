d1157c7bc845b4dafa46f9499998aa98a3e78205
Theresa Wolff <theresawolff4@gmail.com>
2024-02-16 10:24:15 +0000

    Add the logout route to the url_patterns

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -24,11 +24,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/logout/', logout_route)
     # our logout route has to be above the default one to be matched first
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
+    path('', root_route),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
