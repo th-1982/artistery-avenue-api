95bc99b1b1a7ac2bad33bb8e2d6456401adc128c
Theresa Wolff <theresawolff4@gmail.com>
2024-02-19 22:27:49 +0000

    Update the models.py, serializers.py and views.py of contact app

--- contacts/models.py
+++ contacts/models.py
@@ -6,15 +6,11 @@ class Contact(models.Model):
     """
     Comment model, related to User
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    reason = models.CharField(max_length=50)
-    content = models.TextField(max_length=255)
+    email = models.CharField(max_length=254)  
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-created_at"]
 
     def __str__(self):
-        return f"{self.owner} : {self.reason}"
-
+        return self.name
