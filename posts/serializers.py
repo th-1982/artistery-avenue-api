1e69fd1202c82ded980c20619a40d2d560aaa919
Theresa Wolff <theresawolff4@gmail.com>
2024-02-25 22:29:20 +0000

    Add bookmark_id to PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -79,5 +79,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
-            'like_id', 'likes_count', 'comments_count',
+            'like_id', 'bookmark_id', 'likes_count', 'comments_count',
         ]
